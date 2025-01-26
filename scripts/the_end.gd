cef698783e8f9697e76cb2b0748702372a8ab3ad
Gediminas Lelešius <g@lelesius.eu>
2025-01-26 18:28:38 +0200

    allow exiting back to main menu from end screen

--- scripts/the_end.gd
+++ scripts/the_end.gd
@@ -1,4 +1,11 @@
 extends Control
 
+func _input(event: InputEvent) -> void:
+	if event.is_action("ui_accept"):
+		back()
+		
 func _on_back_button_pressed() -> void:
+	back()
+	
+func back() -> void:
 	GameManager.instance(self).start_menu()
