d051df38e0864781bc6a25fc930946bfa68c6184
Gediminas Lelešius <g@lelesius.eu>
2025-01-26 20:54:56 +0200

    fix camera glich on scene reload

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -4,7 +4,13 @@ extends Camera2D
 @onready var minPos = get_viewport_rect().size / 2
 @onready var maxPos = manager.level_size_in_pixels() - get_viewport_rect().size / 2
 
+func _ready() -> void:
+	update_position()
+
 func _physics_process(_delta: float) -> void:
+	update_position()
+	
+func update_position():
 	var player = manager.player
 	if player:
 		position = player.position.clamp(minPos, maxPos)
